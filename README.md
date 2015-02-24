e50de48353a404f594ccc33762fffb4db9a77f8f
U-Computer\Pi Joules <lchan1994@yahoo.com>
2015-02-24 03:02:05 -0500

    added lots o stuff

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ A chrome extension that will turn those cheeky websites into Reddit 420 blaze it
 ```sh
 $ git clone https://github.com/PiJoules/MLG-ifier.git
 ```
-2) To install the extension, enter `chrome://extensions/` in the address bar. Click `Load unpacked extension...` andenter the path to the directory of the cloned repo. A fedora should appear in the upper right corner of the browser next to any other extensions.
+2) To install the extension, enter `chrome://extensions/` in the address bar. Click `Load unpacked extension...` and enter the path to the directory of the cloned repo. A fedora should appear in the upper right corner of the browser next to any other extensions.
 
-## What it does
-Activating the MLG-ifier will
-- Change images on the website to more MLG ones
-- Dankify text into stret lingo your no-scopers will understand
-- Airhorn every so often
+## Features
+- Change images on the website to more euphoric ones
+- Dankify text into l33t lingo your no-scopers will understand
+- Play legit sounds every so often
+- run, Sanic, run
+- Additional features from the [mlg-userscript](https://github.com/twerkclan/mlg-userscript)
 
 ## Dependencies
 - [jspos](https://code.google.com/p/jspos/)
+- [mlg-userscript](https://github.com/twerkclan/mlg-userscript)
 
 ## Todo
-- Add more dank memes + words + sounds
+- Add more dank memes + words + sounds (feel free to sugest any that I should include)
--- mlg.js
+++ mlg.js
@@ -1,4 +1,10 @@
-// Edit the html of the page
+
+
+
+
+// only perform certain actions once
+// keep track of if perfomred by adding a unique element to the dom
+var MLGchecker = "xXx_mlg_checker_xXx";
 
 // Replace sources of all images
 var images = [
@@ -31,11 +37,34 @@ var images = [
     "http://i.imgur.com/lnQKKNY.gif",
     "http://i.imgur.com/1Fa8BPq.jpg",
     "http://i.imgur.com/s2RN7HK.jpg",
-    "http://i.imgur.com/3Cn6ug0.png"
+    "http://i.imgur.com/3Cn6ug0.png",
+    "http://files.gamebanana.com/img/ico/sprays/538acd5a7838b.gif" // the sanic one
 ];
-$("img").each(function(){
-	$(this).attr("src", getRandomElement(images)).attr("height","").css("height","auto");
-});
+
+var sounds = [
+    "airhorn.mp3",
+    "If You Know What I Mean.m4a",
+    "DAMN SON WHERED YOU FIND THIS.mp3",
+    "Oh Baby A Triple.mp3",
+    "SMOKE WEEK EVERYDAY.mp3",
+    "SANIC.mp3"
+];
+
+/*
+"Autumn Blaze" colors (straignt from http://www.colourlovers.com/palette/2996458/autumn_blaze)
+Sam Loomis (whiteish): F9F7F6
+Vanilla Scoop (skin colorish): FBF5D7
+run in the sun (yellow orange ish): FFDD49
+Cheddar Please (orangeish): FCBD49
+light green weed: 7ABA71
+*/
+var weedColors = [
+    [249,247,246],
+    [251,245,215],
+    [255,221,73],
+    [252,189,73],
+    [122,186,113]
+];
 
 
 // word replacements
@@ -46,46 +75,136 @@ var treeBank = {
 	/* Adjective, superlative */ "JJS": ["dankinest", "MLG-inest", "( ͡° ͜ʖ ͡°)"],
 	/* Noun, singular */ "NN": ["dankineer", "pineapple", "Gurl", "m8", "m9", "m80", "parody", "subreddit", "meme", "scrub", "mountain dew", "weed", "blunt", "ebola", "fedora", "trilby", "9/11", "( ͡° ͜ʖ ͡°)", "johnbob", "9gag", "9gagger"],
 	/* Noun, plural */ "NNS": ["dankineers", "pineapples", "Gurlz", "m80s", "m89s", "memes", "scrubs", "doritos", "jimmies", "trilbies", "( ͡° ͜ʖ ͡°)", "johbobs", "9gaggers"],
-	/* Proper noun, singular */ "NNP": ["Sp00nr", "Reddit", "Le Reddit Armie", "Snipars", "Vagabonds", "M'lady", "Good Sir", "9/11", "( ͡° ͜ʖ ͡°)", "Milton Dew"],
+	/* Proper noun, singular */ "NNP": ["Sp00nr", "Reddit", "Le Reddit Armie", "Snipars", "Vagabonds", "M'lady", "Good Sir", "9/11", "( ͡° ͜ʖ ͡°)", "Milton Dew", "Megatokyo", "Rush Limbaugh"],
 	/* Verb, past tense */ "VBD": ["rekt", "upboated", "rekm8ed", "rustled", "( ͡° ͜ʖ ͡°)", "robbled", "flustered"],
 	/* Verb, non-3rd person singular present */ "VBP": ["transl8", "feel", "upboat", "upvote", "rip", "rekm8", "rustle", "( ͡° ͜ʖ ͡°)", "robble", "fluster"],
 	/* Verb, 3rd person singular present */ "VBZ": ["rekts", "upboats", "rustles", "( ͡° ͜ʖ ͡°)", "robbles", "flusters"]
 };
 
-// Replace text in divs with only text (and no child HTML elements), and ps
-$("span:not(:has(*)),p:not(:has(*)),div:not(:has(*)),a:not(:has(*))").each(function(){
-	var text = $(this).text();
-	var words = new Lexer().lex(text);
-	var taggedWords = new POSTagger().tag(words);
-	for (var i in taggedWords) {
-		var taggedWord = taggedWords[i];
-		var word = taggedWord[0];
-		var tag = taggedWord[1];
 
-		// 50% chance of changing each word
-		if (tag in treeBank && Math.random() < 0.5){
-			if (Math.random() < 0.1){
-				text = text.replace(word, "xXx_" + word + "_xXx");
-			}
-			else {
-				var randWord = getRandomElement(treeBank[tag]);
+// Only perform these actions once
+if ($("#xXx_mlg_checker_xXx").length === 0){
+    $("img:not(#sanic2fast)").each(function(){
+        $(this).attr("src", getRandomElement(images)).attr("height","").css("height","auto");
+    });
+    
+    // Replace text in divs with only text (and no child HTML elements), and ps
+    $("span:not(:has(*)),p:not(:has(*)),div:not(:has(*)),a:not(:has(*))").each(function(){
+        var text = $(this).text();
+        var words = new Lexer().lex(text);
+        var taggedWords = new POSTagger().tag(words);
+        for (var i in taggedWords) {
+            var taggedWord = taggedWords[i];
+            var word = taggedWord[0];
+            var tag = taggedWord[1];
 
-				// 10% chance of adding xXx_word_xXx to noun
-				if (tag === "NN" || tag === "NNS" || tag === "NNP"){
-					if (Math.random() < 0.05){
-						randWord = "xXx_" + randWord + "_xXx";
-					}
-				}
-				text = text.replace(word, randWord);
-			}
-		}
-	}
-	$(this).text(text);
+            // 50% chance of changing each word
+            if (tag in treeBank && Math.random() < 0.5){
+                if (Math.random() < 0.1){
+                    text = text.replace(word, "xXx_" + word + "_xXx");
+                }
+                else {
+                    var randWord = getRandomElement(treeBank[tag]);
+
+                    // 5% (0.5*0.1) chance of adding xXx_word_xXx to noun
+                    if (tag === "NN" || tag === "NNS" || tag === "NNP"){
+                        if (Math.random() < 0.05){
+                            randWord = "xXx_" + randWord + "_xXx";
+                        }
+                    }
+                    text = text.replace(word, randWord);
+                }
+            }
+        }
+        $(this).text(text);
+    });
+    
+    /* ( ͡° ͜ʖ ͡°) MLG event handling */
+    /* "oi u cheeky cunt 1v1 me" --skrillex */
+    document.addEventListener("click", function(evt) {
+        evt = (evt || event);
+        mark(evt.clientX, evt.clientY);
+        causeSeizure();
+    });
+
+    window.addEventListener("scroll", function(evt) {
+        /* rate limit for smooth scrolling */
+        if (Date.now() - scrollTime < 50) return;
+        scrollTime = Date.now();
+        hornAudio.pause();
+        hornAudio.currentTime = 0;
+        hornAudio.play();
+        intensifyScrolling();
+    });
+
+    window.addEventListener("contextmenu", function(evt) {
+        damnAudio.pause();
+        damnAudio.currentTime = 0;
+        damnAudio.play();    
+        causeSeizure();
+    });
+
+    /* ( ͡° ͜ʖ ͡°) MLG copy-and-paste solutions from the internet */
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = "* { font-family: \"Comic Sans MS\" !important; }";
+    document.body.appendChild(css);
+}
+
+
+function addSound(id, src){
+    if ($("#" + id).length)
+        changeSound(id,src)
+    else
+        $("body").append('<audio id="' + id + '" src="' + src + '" preload="auto"></audio>');
+}
+
+function removeSound(id){
+    $("#" + id).remove();
+}
+
+function playSound(id){
+    $('#' + id)[0].play();
+}
+
+function changeSound(id, sound){
+    $("#" + id).attr("src", sound);
+}
+
+function getRandomElement(items){
+	return items[Math.floor(Math.random()*items.length)];
+}
+
+function runSanic(){
+    $("#sanic2fast").animate({left: $(window).width()}, 2000, "linear", function(){
+        $("#sanic2fast").css("left","-256px");
+    });
+}
+
+
+// Audio tag for playing sound
+var randSound = getRandomElement(sounds);
+var initRandSoundID = "initSound";
+if (typeof chrome.extension !== "undefined")
+    addSound(initRandSoundID, chrome.extension.getURL('sounds/' + randSound));
+else
+    addSound(initRandSoundID, 'sounds/' + randSound);
+playSound(initRandSoundID);
+
+if (!$("#sanic2fast").length)
+    $("body").append("<img id='sanic2fast' src='http://files.gamebanana.com/img/ico/sprays/538acd5a7838b.gif' />");
+
+$("#sanic2fast").css({
+    "position": "fixed",
+    "top": "calc(50% - 128px)",
+    "left": "-256px",
+    "z-index": "10000000"
 });
 
-// Audia tag for playing sound
-$("body").append('<audio id="audiotag1" src="' + chrome.extension.getURL('sounds/airhorn.mp3') + '" preload="auto"></audio>');
-playAirhorn();
+if (randSound === "SANIC.mp3"){
+    runSanic();
+}
+
 
 // Change background image of body
 $("body").css({
@@ -95,16 +214,23 @@ $("body").css({
 });
 
 window.setInterval(function(){
-	// 1% chance of playing airhorn every 5 seconds
+	// 1% chance of playing random sound every 5 seconds
 	if (Math.random() < 0.01){
-		playAirhorn();
+        var nextRandSound = getRandomElement(sounds);
+        if (typeof chrome.extension !== "undefined")
+            changeSound(initRandSoundID, chrome.extension.getURL('sounds/' + nextRandSound));
+        else
+            changeSound(initRandSoundID, 'sounds/' + nextRandSound);
+		playSound(initRandSoundID);
+        
+        if (nextRandSound === "SANIC.mp3"){
+            runSanic();
+        }
 	}
 }, 5000);
 
-function playAirhorn(){
-	document.getElementById('audiotag1').play();
-};
 
-function getRandomElement(items){
-	return items[Math.floor(Math.random()*items.length)];
-};
+// add the mlg checker
+if ($("#xXx_mlg_checker_xXx").length === 0)
+    $("body").append("<div id='xXx_mlg_checker_xXx'></div>");
+
